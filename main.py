ce41d992ca4accc63a6f5f956e7b52fb78c99bea
Dekoder-py-school <kyle.bendall@marcellin.school.nz>
2025-06-18 19:51:26 +1200

    add task creation and listing tasks using sqlite

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 MENU_PROMPT = """Choose an option by entering a number:
 1. Add a new task
 2. List all tasks
@@ -18,35 +20,42 @@ displayed when listed like this:
 """
 
 
-def add_task(task):
+def add_task(cursor, task):
+    cursor.execute("INSERT INTO tasks (task) VALUES (?)", (task,))
+    cursor.connection.commit()
+
+def list_tasks(cursor):
+    tasks = cursor.execute("SELECT id, task, completed FROM tasks").fetchall()
+    for task in tasks:
+        status = "[X]" if task[2] else "[ ]"
+        print(f"{task[0]}. {status} {task[1]}")
+
+def complete_task(cursor, task_id):
     pass
 
-def list_tasks():
-    pass
-
-def complete_task(task_id):
-    pass
-
-def delete_task(task_id):
+def delete_task(cursor, task_id):
     pass
 
 def main():
+    con = sqlite3.connect("tasks.sqlite")
+    cursor = con.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS tasks(id INTEGER PRIMARY KEY, task TEXT, completed BOOLEAN DEFAULT 0)")
     print("Welcome to the Dekoder-py-school to do app!")
     choice = input(MENU_PROMPT)
     while choice != "5":
         if choice == "1":
             task = input("Enter the task: ")
-            add_task(task)
+            add_task(cursor, task)
         elif choice == "2":
-            list_tasks()
+            list_tasks(cursor)
         elif choice == "3":
-            list_tasks()
+            list_tasks(cursor)
             task_id = input("Enter the task number to complete: ") # TODO: convert to int with error handling
-            complete_task(task_id)
+            complete_task(cursor, task_id)
         elif choice == "4":
-            list_tasks()
+            list_tasks(cursor)
             task_id = input("Enter the task number to complete: ")  # TODO: convert to int with error handling
-            delete_task(task_id)
+            delete_task(cursor, task_id)
         else:
             print("Invalid option.")
         choice = input(MENU_PROMPT)
